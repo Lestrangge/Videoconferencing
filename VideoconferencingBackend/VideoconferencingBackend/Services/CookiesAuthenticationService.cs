fd0901369b19a2c79cc5e1195e13685cf2af0ca5
Anton Pepelyaev <lestrangge@mail.ru>
2019-03-28 18:35:16 +0300

    Cookies auth for browsers implementation

---
+++ VideoconferencingBackend/VideoconferencingBackend/Services/CookiesAuthenticationService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VideoconferencingBackend.Interfaces;
+using VideoconferencingBackend.Models.DBModels;
+
+namespace VideoconferencingBackend.Services
+{
+    public class CookiesAuthenticationService : ICustomAuthenticationService
+    {
+        private readonly IRepository<User> _usersRepository;
+        private readonly IHasherService _hasher;
+        private readonly IHttpContextAccessor _accessor;
+
+        public CookiesAuthenticationService(IRepository<User> usersRepository, IHasherService hasher, IHttpContextAccessor accessor)
+        {
+            _hasher = hasher;
+            _usersRepository = usersRepository;
+            _accessor = accessor;
+        }
+
+        public async Task<IActionResult> Signup(User credentials)
+        {
+            if (await _usersRepository.Get(credentials.Login) != null)
+                return new BadRequestObjectResult("Login is already taken");
+            if (Zxcvbn.Zxcvbn.MatchPassword(credentials.Password).Score < 2)
+                return new BadRequestObjectResult("Paasword is weak");
+
+            credentials.Password = _hasher.Hash(credentials.Password);
+            await _usersRepository.Create(credentials);
+            await SetCookies(credentials);
+
+            return new OkResult();
+        }
+
+        public async Task<IActionResult> Login(User credentials)
+        {
+            var user = await _usersRepository.Get(credentials.Login);
+            if (user == null)
+                return new BadRequestObjectResult("User not found");
+            if(user.Password != _hasher.Hash(credentials.Password))
+                return new BadRequestObjectResult("Wrong password");
+            await SetCookies(user);
+            return new OkResult();
+        }
+
+        private Task SetCookies(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            return _accessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+        }
+    }
+}
---
+++ VideoconferencingBackend/VideoconferencingBackend/Services/Sha256Hasher.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using VideoconferencingBackend.Interfaces;
+
+namespace VideoconferencingBackend.Services
+{
+    public class Sha256Hasher : IHasherService
+    {
+        public string Hash(string toHash)
+        {
+            if (toHash == null)
+                return null;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(toHash));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
