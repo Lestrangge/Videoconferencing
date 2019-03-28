b0451f238905c12b46f8599ae592bab2fa3a22f6
Anton Pepelyaev <lestrangge@mail.ru>
2019-03-28 18:37:09 +0300

    Token auth implementation for native apps

---
+++ VideoconferencingBackend/VideoconferencingBackend/Interfaces/ITokenGeneratorService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using VideoconferencingBackend.Models.DBModels;
+
+namespace VideoconferencingBackend.Interfaces
+{
+    public interface ITokenGeneratorService
+    {
+        string GenerateToken(User user);
+    }
+}
