e9f0d8eae3c837cbad4eea22392daeb5af41bb31
Anton Pepelyaev <lestrangge@mail.ru>
2019-04-07 05:02:33 +0300

    Added abstraction for smtp client

---
+++ VideoconferencingBackend/VideoconferencingBackend/Adapters/SmtpClientAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using VideoconferencingBackend.Interfaces.Adapters;
+
+namespace VideoconferencingBackend.Wrappers
+{
+    public class SmtpClientAdapter : ISmtpClientAdapter
+    {
+        ///<inheritdoc/>
+        public Task SendMailAsync(MailMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VideoconferencingBackend/VideoconferencingBackend/Interfaces/Adapters/ISmtpClientAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace VideoconferencingBackend.Interfaces.Adapters
+{
+    public interface ISmtpClientAdapter
+    {
+        /// <summary>
+        /// Sends an email 
+        /// </summary>
+        /// <returns></returns>
+        Task SendMailAsync(MailMessage message);
+    }
+}
